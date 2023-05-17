e3483cc99755c7ddfebd2d84407af2c8417e20fa
Brian Forbes <brian@Brians-MacBook-Pro.local>
2023-05-17 09:33:38 -0400

    Hello World test at first get route works

--- character/src/main/java/codex/java/character/controllers/CharacterController.java
+++ character/src/main/java/codex/java/character/controllers/CharacterController.java
@@ -1,8 +1,14 @@
 package codex.java.character.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CharacterController {
-    
+
+    @GetMapping("/")
+    public String first(){
+        return "Hello World";
+    }
+
 }
