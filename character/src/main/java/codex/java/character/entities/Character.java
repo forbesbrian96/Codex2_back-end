a362a66331ed0ea30253d80db616999b7f5100d2
Brian Forbes <brian@Brians-MacBook-Pro.local>
2023-05-17 10:06:35 -0400

    completed etity properties through ability scores

--- character/src/main/java/codex/java/character/entities/Character.java
+++ character/src/main/java/codex/java/character/entities/Character.java
@@ -17,4 +17,104 @@ public class Character {
     
     @Id //Denotes this property as the id/primary key
     @GeneratedValue(strategy = GenerationType.AUTO) //auto generates
+    public Integer id;
+
+        ///////////////////////////////////////////////
+        // CHARACTER DESCRIPTORS
+        ///////////////////////////////////////////////
+
+
+    public String characterName;
+
+    public Integer maxHitPoints;
+
+    public Integer currentHitPoints;
+
+    public Integer tempHitPoints;
+
+    public Integer AC;
+
+    public Integer intitiative;
+
+    public Integer speed;
+
+    public Integer baseSpeed;
+
+    public Integer age;
+
+    public String height;
+
+    public Integer weight;
+
+    public String eyes;
+
+    public String hair;
+
+    public String skin;
+
+    public String race;
+
+    public String characterClass;
+
+    public String alignment;
+
+    public String background;
+
+    public Integer experience;
+
+    public Integer level;
+
+    public Integer profBonus;
+
+        ///////////////////////////////////////////////
+        // Ability Scores
+        ///////////////////////////////////////////////
+
+    public Integer strScore;
+
+    public Integer strMod;
+
+    public Integer strMisc;
+
+    public Integer strSave;
+
+    public Integer dexScore;
+
+    public Integer dexMod;
+
+    public Integer dexMisc;
+
+    public Integer dexSave;
+    
+    public Integer conScore;
+
+    public Integer conMod;
+
+    public Integer conMisc;
+
+    public Integer conSave;
+
+    public Integer intScore;
+
+    public Integer intMod;
+
+    public Integer intMisc;
+
+    public Integer intSave;
+
+    public Integer wisScore;
+
+    public Integer wisMod;
+
+    public Integer wisMisc;
+
+    public Integer wisSave;
+
+    public Integer chaScore;
+
+    public Integer chaMod;
+
+    public Integer chaMisc;
+
+    public Integer chaSave;
 }
