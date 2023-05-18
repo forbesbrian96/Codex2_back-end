822a75df3fe1eec5cd7f642a5eee07d3eb27d64a
Brian Forbes <brian@Brians-MacBook-Pro.local>
2023-05-18 15:46:37 -0400

    created index and show routes in CharacterController

--- character/src/main/java/codex/java/character/controllers/CharacterController.java
+++ character/src/main/java/codex/java/character/controllers/CharacterController.java
@@ -1,14 +1,43 @@
 package codex.java.character.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import codex.java.character.repositories.CharacterRepository;
+import codex.java.character.entities.Character;
 
 @RestController
 public class CharacterController {
 
-    @GetMapping("/")
-    public String first(){
-        return "Hello World";
+    // Property to hold Character Repository
+    public CharacterRepository Characters;
+
+    // Receive Character Repository via Dependency Injection
+    public CharacterController(CharacterRepository Characters){
+        this.Characters = Characters;
     }
 
+    /////////////////////////////////////////////////////////////////
+    // INDEX
+    /////////////////////////////////////////////////////////////////
+
+    // Get request to /character
+    @GetMapping("/character")
+    public List<Character> index(){
+        return Characters.findAll();
+    }
+
+    /////////////////////////////////////////////////////////////////
+    // SHOW
+    /////////////////////////////////////////////////////////////////
+
+    // Get request to /character/:id
+    @GetMapping("/character/:id")
+    public Optional<Character> show(@PathVariable Integer id){
+        return Characters.findById(id);
+    }
 }
