42de8dc21c60645642441c78a18a5fe8d5a69cb5
Brian Forbes <brian@Brians-MacBook-Pro.local>
2023-05-18 16:01:04 -0400

    added put request to CharacterController

--- character/src/main/java/codex/java/character/controllers/CharacterController.java
+++ character/src/main/java/codex/java/character/controllers/CharacterController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,4 +54,86 @@ public class CharacterController {
         Characters.save(newCharacter); // Create the new Character
         return Characters.findAll();
     }
+
+    /////////////////////////////////////////////////////////////////
+    // UPDATE
+    /////////////////////////////////////////////////////////////////
+
+    // Put request to /characters/{id}
+    @PutMapping("/characters/{id}")
+    public List<Character> update(@RequestBody Character fixCharacter, @PathVariable Integer id){
+        // Find character by it's id then use map to update it
+        Characters.findById(id).map(character -> {
+
+            character.setCharacterName(fixCharacter.getCharacterName());
+            character.setMaxHitPoints(fixCharacter.getMaxHitPoints());
+            character.setCurrentHitPoints(fixCharacter.getCurrentHitPoints());
+            character.setTempHitPoints(fixCharacter.getTempHitPoints());
+            character.setAC(fixCharacter.getAC());
+            character.setInitiative(fixCharacter.getInitiative());
+            character.setSpeed(fixCharacter.getSpeed());
+            character.setBaseSpeed(fixCharacter.getBaseSpeed());
+            character.setAge(fixCharacter.getAge());
+            character.setHeight(fixCharacter.getHeight());
+            character.setWeight(fixCharacter.getWeight());
+            character.setEyes(fixCharacter.getEyes());
+            character.setHair(fixCharacter.getHair());
+            character.setSkin(fixCharacter.getSkin());
+            character.setRace(fixCharacter.getRace());
+            character.setCharacterClass(fixCharacter.getCharacterClass());
+            character.setAlignment(fixCharacter.getAlignment());
+            character.setBackground(fixCharacter.getBackground());
+            character.setExperience(fixCharacter.getExperience());
+            character.setLevel(fixCharacter.getLevel());
+            character.setProfBonus(fixCharacter.getProfBonus());
+            character.setStrScore(fixCharacter.getStrScore());
+            character.setStrMod(fixCharacter.getStrMod());
+            character.setStrMisc(fixCharacter.getStrMisc());
+            character.setStrSave(fixCharacter.getStrSave());
+            character.setDexScore(fixCharacter.getDexScore());
+            character.setDexMod(fixCharacter.getDexMod());
+            character.setDexMisc(fixCharacter.getDexMisc());
+            character.setDexSave(fixCharacter.getDexSave());
+            character.setConScore(fixCharacter.getConScore());
+            character.setConMod(fixCharacter.getConMod());
+            character.setConMisc(fixCharacter.getConMisc());
+            character.setConSave(fixCharacter.getConSave());
+            character.setIntScore(fixCharacter.getIntScore());
+            character.setIntMod(fixCharacter.getIntMod());
+            character.setIntMisc(fixCharacter.getIntMisc());
+            character.setIntSave(fixCharacter.getIntSave());
+            character.setWisScore(fixCharacter.getWisScore());
+            character.setWisMod(fixCharacter.getWisMod());
+            character.setWisMisc(fixCharacter.getWisMisc());
+            character.setWisSave(fixCharacter.getWisSave());
+            character.setChaScore(fixCharacter.getChaScore());
+            character.setChaMod(fixCharacter.getChaMod());
+            character.setChaMisc(fixCharacter.getChaMisc());
+            character.setChaSave(fixCharacter.getChaSave());
+            character.setAcrobatics(fixCharacter.getAcrobatics());
+            character.setAnimalHandling(fixCharacter.getAnimalHandling());
+            character.setArcana(fixCharacter.getArcana());
+            character.setAthletics(fixCharacter.getAthletics());
+            character.setDeception(fixCharacter.getDeception());
+            character.setHistory(fixCharacter.getHistory());
+            character.setInsight(fixCharacter.getInsight());
+            character.setIntimidation(fixCharacter.getIntimidation());
+            character.setInvestigation(fixCharacter.getInvestigation());
+            character.setMedicine(fixCharacter.getMedicine());
+            character.setNature(fixCharacter.getNature());
+            character.setPerception(fixCharacter.getPerception());
+            character.setPerformance(fixCharacter.getPerformance());
+            character.setPersuasion(fixCharacter.getPersuasion());
+            character.setReligion(fixCharacter.getReligion());
+            character.setSleightOfHand(fixCharacter.getSleightOfHand());
+            character.setStealth(fixCharacter.getStealth());
+            character.setSurvival(fixCharacter.getSurvival());
+            character.setLanguages(fixCharacter.getLanguages());
+            character.setProficiencies(fixCharacter.getProficiencies());
+            
+            return Characters.save(character);
+        });
+        
+        return Characters.findAll();
+}
 }
